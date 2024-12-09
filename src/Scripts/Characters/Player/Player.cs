a9136109aebfe476746c3a69df8cdc2d7d1f2ff3
Jakub Bielecki <kuba.bielecki@outlook.com>
2024-12-09 20:16:33 +0100

    develop/refactored player moving.

--- src/Scripts/Characters/Player/Player.cs
+++ src/Scripts/Characters/Player/Player.cs
@@ -1,17 +1,49 @@
 using Godot;
 public partial class Player : CharacterBody3D
 {
+    [ExportGroup("Required Nodes")]
+    [Export] private AnimationPlayer _animationPlayerNode;
+    [Export] private Sprite3D _spriteNode;
+
     private Vector2 _direction = new();
+
+    public override void _Ready()
+    {
+        _animationPlayerNode.Play(GameConstants.ANIM_IDLE);
+    }
 
     public override void _PhysicsProcess(double delta)
     {
         Velocity = new(_direction.X, 0, _direction.Y);
         Velocity *= 5;
         MoveAndSlide();
+        Flip();
     }
 
     public override void _Input(InputEvent @event)
     {
-        _direction = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+        _direction = Input.GetVector(GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT,
+            GameConstants.INPUT_MOVE_FORWARD, GameConstants.INPUT_MOVE_BACKWARD);
+
+        if (_direction == Vector2.Zero)
+        {
+            _animationPlayerNode.Play(GameConstants.ANIM_IDLE);
+        }
+        else
+        {
+            _animationPlayerNode.Play(GameConstants.ANIM_MOVE);
+        }
+    }
+
+    private void Flip()
+    {
+        var isNotMovingHorizontally = Velocity.X == 0;
+
+        if (isNotMovingHorizontally)
+        {
+            return;
+        }
+
+        _spriteNode.FlipH = Velocity.X < 0;
     }
 }
